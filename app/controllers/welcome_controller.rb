7c1fde61438103a4d3140b13596956df09bf5059
clody01 <tseoue01@gamil.com>
2015-04-20 11:21:11 +0000

    built navigation bar and set root page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def faq
+  end
+
+  def pricing
+  end
+
+  def features
+  end
+
+  def content
+  end
+
+  def about
+  end
+end
