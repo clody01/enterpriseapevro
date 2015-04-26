0a5ea1bb7e1c96c9bd7dadfd17185a543a2c3109
clody01 <tseoue01@gamil.com>
2015-04-26 15:52:45 +0000

    ajout recherche par date

---
+++ app/models/invoice_search.rb
@@ -0,0 +1,18 @@
+class InvoiceSearch
+  attr_reader :date_from , :date_to
+  def initialize(params)
+    params ||={}
+    @date_from=parsed_date(params[:date_from ],7.days.ago.to_date.strftime("%d/%m/%y"))
+    @date_to=parsed_date(params[:date_to ],Date.today.strftime("%d/%m/%y"))
+  end
+  def scope
+    Invoice.where('date BETWEEN ? AND ?',@date_from, @date_to)
+  end
+  
+  private
+  def parsed_date(date_string,default)
+      Date.parse(date_string).strftime("%d/%m/%y")
+  rescue ArgumentError, TypeError
+    default
+  end
+end
